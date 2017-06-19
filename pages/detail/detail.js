33d84d9590d08830a9d3d5dfd263ac19c269e37a
Jack <me@jarrekk.com>
2017-06-19 17:20:24 +0800

    add load comment

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -7,12 +7,64 @@ Page({
     contentShow: false,
     commentShow: false,
     detail: null,
-    comments: null
+    comments: null,
+    loadMore: false,
+    loadMoreLoading: false,
+    page: 0,
+    comment_list: []
   },
   //事件处理函数
   bindViewTap: function() {
     wx.navigateTo({
       url: '../logs/logs'
+    })
+  },
+  loadmore: function () {
+    var that = this;
+    that.setData({
+      loadMore: false,
+      loadMoreLoading: true
+    })
+    var comments = that.data.comments;
+    var comment_list = that.data.comment_list;
+    var pageRange = comment_list.slice(that.data.page * 5, (that.data.page + 1) * 5);
+    wx.request({
+      url: app.globalData.APIServer + '/list/[' + pageRange + ']',
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        console.log(res.data.data)
+        comments = comments.concat(res.data.data)
+        for (var i = 0; i < comments.length; i++) {
+          if (comments[i].deleted == true) {
+            WxParse.wxParse('comment' + i, 'html', 'This comment has been deleted.', that, 0);
+          } else {
+            WxParse.wxParse('comment' + i, 'html', comments[i].text, that, 0);
+          }
+          if (i === comments.length - 1) {
+            WxParse.wxParseTemArray("commentArray", 'comment', comments.length, that)
+          }
+        }
+        that.setData({
+          comments: comments,
+          commentShow: true,
+          page: that.data.page + 1,
+          comment_list: comment_list
+        })
+        console.log(that.data.page, that.data.comment_list)
+        if ((that.data.page + 0) * 5 >= that.data.comment_list.length) {
+          that.setData({
+            loadMore: false,
+            loadMoreLoading: false
+          })
+        } else {
+          that.setData({
+            loadMore: true,
+            loadMoreLoading: false
+          })
+        }
+      }
     })
   },
   onLoad: function (options) {
@@ -32,31 +84,48 @@ Page({
           detail: res.data.data,
           contentShow: true
         })
-        var first_page = [];
         if (res.data.data.kids) {
-        first_page = res.data.data.kids.slice(0, 5);
-        var comments = []
-        wx.request({
-          url: app.globalData.APIServer + '/list/[' + first_page + ']',
-          header: {
-            'content-type': 'application/json'
-          },
-          success: function (res) {
-            console.log(res.data.data)
-            comments = comments.concat(res.data.data)
-            for (var i = 0; i < comments.length; i++) {
-              WxParse.wxParse('comment' + i, 'html', comments[i].text, that, 0);
-              if (i === comments.length - 1) {
-                WxParse.wxParseTemArray("commentArray", 'comment', comments.length, that)
+          var comment_list = res.data.data.kids;
+          var pageRange = res.data.data.kids.slice(that.data.page * 5, (that.data.page + 1) * 5);
+          var comments = []
+          wx.request({
+            url: app.globalData.APIServer + '/list/[' + pageRange + ']',
+            header: {
+              'content-type': 'application/json'
+            },
+            success: function (res) {
+              console.log(res.data.data)
+              comments = comments.concat(res.data.data)
+              for (var i = 0; i < comments.length; i++) {
+                if (comments[i].deleted == true) {
+                  WxParse.wxParse('comment' + i, 'html', 'This comment has been deleted.', that, 0);
+                } else {
+                  WxParse.wxParse('comment' + i, 'html', comments[i].text, that, 0);
+                }
+                if (i === comments.length - 1) {
+                  WxParse.wxParseTemArray("commentArray", 'comment', comments.length, that)
+                }
+              }
+              that.setData({
+                comments: comments,
+                commentShow: true,
+                page: that.data.page + 1,
+                comment_list: comment_list
+              })
+              console.log(that.data.page, that.data.comment_list)
+              if ((that.data.page + 0) * 5 >= that.data.comment_list.length) {
+                that.setData({
+                  loadMore: false,
+                  loadMoreLoading: false
+                })
+              } else {
+                that.setData({
+                  loadMore: true,
+                  loadMoreLoading: false
+                })
               }
             }
-            that.setData({
-              comments: comments,
-              commentShow: true
-            })
-
-          }
-        })
+          })
         } else {
           that.setData({
             commentShow: true
